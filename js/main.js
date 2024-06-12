9b6fa596ba48a98bf7139e089bf192d6de04a0e4
Busola Tom <busolatom@gmail.com>
2024-06-12 03:05:23 +0000

    Auto Slider added to page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
+// Swiper js
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  swiper.destroy();
-
-  
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
