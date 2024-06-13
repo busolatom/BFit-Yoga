7d0433c5790e01feb7e2e13b919e98573773d35e
Busola Tom <busolatom@gmail.com>
2024-06-13 13:33:51 +0000

    Project completed

--- js/main.js
+++ js/main.js
@@ -15,3 +15,17 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// dropdown js
+// Optional: Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+}
